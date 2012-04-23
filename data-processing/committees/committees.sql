190e5f8cd8c851f4f49f8b616a5d61490fe37b42
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-04-23 14:57:09 -0500

    Correct misspelling of Barack.

--- data-processing/committees/committees.sql
+++ data-processing/committees/committees.sql
@@ -15,7 +15,7 @@ WITH (OIDS=FALSE);
 -- ----------------------------
 BEGIN;
 INSERT INTO "committees" VALUES ('C00431171', 'Romney for President Inc.', 'Mitt Romney', 'Romney');
-INSERT INTO "committees" VALUES ('C00431445', 'Obama for America', 'Barak Obama', 'Obama');
+INSERT INTO "committees" VALUES ('C00431445', 'Obama for America', 'Barack Obama', 'Obama');
 INSERT INTO "committees" VALUES ('C00495820', 'Ron Paul 2012 President Campaign Committee Inc.', 'Ron Paul', 'Paul');
 INSERT INTO "committees" VALUES ('C00496497', 'Newt 2012', 'Newt Gingrich', 'Gingrich');
 INSERT INTO "committees" VALUES ('C00496034', 'Rick Santorum for President Inc.', 'Rick Santorum', 'Santorum');
