532c1bfa6f79e51a99083508630bab6369d31869
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-04-23 15:01:13 -0500

    Updated committee sql to not use constraints.

--- data-processing/committees/committees.sql
+++ data-processing/committees/committees.sql
@@ -6,7 +6,8 @@ CREATE TABLE "committees" (
 	"committee_id" varchar(32) NOT NULL,
 	"full_name" varchar(512) NOT NULL,
 	"candidate_name" varchar(512) NOT NULL,
-	"short_name" varchar(512) NOT NULL
+	"short_name" varchar(512) NOT NULL,
+	PRIMARY KEY ("committees")
 )
 WITH (OIDS=FALSE);
 
@@ -21,13 +22,4 @@ INSERT INTO "committees" VALUES ('C00496497', 'Newt 2012', 'Newt Gingrich', 'Gin
 INSERT INTO "committees" VALUES ('C00496034', 'Rick Santorum for President Inc.', 'Rick Santorum', 'Santorum');
 COMMIT;
 
--- ----------------------------
---  Primary key structure for table "committees"
--- ----------------------------
-ALTER TABLE "committees" ADD CONSTRAINT "committees_pkey" PRIMARY KEY ("committee_id") NOT DEFERRABLE INITIALLY IMMEDIATE;
-
--- ----------------------------
---  Indexes structure for table "committees"
--- ----------------------------
-CREATE UNIQUE INDEX "committee_index" ON "committees" USING btree(committee_id ASC NULLS LAST);
 
